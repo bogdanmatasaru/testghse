0cc5d6fe58001c9d48651a616953506687f65f76
bogdanmatasaru <bogdan.matasaru@yahoo.com>
2023-05-16 10:40:32 +0300

    comm

---
+++ test cc 33/test_cc_33App.swift
@@ -0,0 +1,17 @@
+//
+//  test_cc_33App.swift
+//  test cc 33
+//
+//  Created by Bogdan on 16.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct test_cc_33App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
